9668a03b76cc7b1ea7977b03ff5148972008572d
stephdraggo <steph.valais@gmx.com>
2021-05-24 16:59:02 +1000

    yes

---
+++ Assets/Ragdoll/Scripts/CamMovement.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ragdoll
+{
+    public class CamMovement : MonoBehaviour
+    {
+        public static CamMovement Instance;
+
+        public bool hasSelected;
+        
+        [SerializeField] private float speed = 1;
+
+        [SerializeField] private Transform crosshair;
+        public Transform Crosshair => crosshair;
+
+        void Awake()
+        {
+            Instance = this;
+            hasSelected = false;
+        }
+
+        void FixedUpdate()
+        {
+            transform.position += GetInput();
+        }
+
+        private Vector3 GetInput()
+        {
+            float x = 0;
+            float z = 0;
+            if (Input.GetKey(KeyCode.A)) x--;
+            if (Input.GetKey(KeyCode.D)) x++;
+            if (Input.GetKey(KeyCode.W)) z++;
+            if (Input.GetKey(KeyCode.S)) z--;
+            float y = -Input.mouseScrollDelta.y;
+            return new Vector3(x, y, z) * (speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Ragdoll/Scripts/SelectableBase.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ragdoll
+{
+    public class SelectableBase : MonoBehaviour
+    {
+        private bool selected;
+        private Transform parent;
+        private new Rigidbody rigidbody;
+
+        [SerializeField] private SkinnedMeshRenderer mesh;
+
+        [SerializeField] private Material baseMat, highlighted;
+
+        void OnValidate()
+        {
+            mesh.material = baseMat;
+            parent = transform.parent;
+            rigidbody = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            if (selected)
+            {
+                transform.position = transform.parent.position;
+
+                if (Input.GetMouseButtonUp(0)) Drop();
+            }
+        }
+
+        private void OnMouseEnter()
+        {
+            if (!selected && !CamMovement.Instance.hasSelected)
+            {
+                mesh.material = highlighted;
+            }
+        }
+
+        private void OnMouseExit()
+        {
+            mesh.material = baseMat;
+        }
+
+        private void OnMouseOver()
+        {
+            if (!selected && !CamMovement.Instance.hasSelected)
+            {
+                if (Input.GetMouseButton(0)) Select();
+            }
+        }
+
+        private void Select()
+        {
+            selected = true;
+            transform.SetParent(CamMovement.Instance.Crosshair);
+            CamMovement.Instance.hasSelected = true;
+            rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        }
+
+        private void Drop()
+        {
+            selected = false;
+            transform.SetParent(parent);
+            CamMovement.Instance.hasSelected = false;
+            rigidbody.constraints = RigidbodyConstraints.None;
+        }
+    }
+}
